64b054b49fa6f1ced1d4047b8138a3f000f0019e
RU063 <rashedinislam.06@gmail.com>
2024-08-31 23:27:15 +0600

    first server running

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('ByteBlog server is running')
+})
+
+app.listen(port, () => {
+  console.log('ByteBlog server is running');
+  
+})
